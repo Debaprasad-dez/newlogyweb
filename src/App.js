b16533ff678a9bab82dbd71a9727f42a76417fb5
Debaprasad-dez <debaprasadpaul033@gmail.com>
2023-08-21 19:35:17 +0530

    react page deploy 9

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Contact from "./Components/Contact";
-import Recentnews from "./Components/Recentnews";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
