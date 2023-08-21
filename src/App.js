f8382c8199e139fa795b5680e7e70ff331d788c0
Debaprasad-dez <debaprasadpaul033@gmail.com>
2023-08-21 14:45:25 +0530

    react page deploy 8

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Contact from "./Components/Contact";
+import Recentnews from "./Components/Recentnews";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
       <Routes>
-        <Route path="/contact" element={<Contact />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,7 +2,21 @@ function Contact() {
   return (
     <div className="contactDiv">
       <div>
-        <h1 className="dummytext">This is Contact Component</h1>
+        <h1>This is Contact Component</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eaque
+          eius veritatis consequuntur voluptatibus illum nihil ex atque modi ad,
+          dolor dolores consectetur non, laboriosam magnam reiciendis libero
+          obcaecati debitis. Neque distinctio, accusamus laboriosam asperiores
+          nemo sequi harum similique esse eius sed natus consectetur delectus
+          iusto reprehenderit totam ipsa expedita tempore aperiam molestiae
+          deserunt libero. Harum nemo, dolores quaerat facere omnis soluta
+          recusandae molestias debitis rerum eligendi est deserunt nostrum ipsa
+          assumenda numquam quo accusamus molestiae id quia nisi! Eos
+          voluptatem, veritatis totam vitae id et placeat illo in itaque!
+          Ducimus, eos. Dolore corporis cupiditate obcaecati corrupti ullam sed
+          tempora!
+        </p>
       </div>
     </div>
   );
